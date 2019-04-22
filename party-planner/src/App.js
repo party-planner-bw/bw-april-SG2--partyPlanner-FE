eba1a204f88ee80d0c67da91552103d0cdd50ab5
Brennon Nuckols <brennuck@gmail.com>
2019-04-22 10:35:09 -0600

    Login page

--- party-planner/src/App.js
+++ party-planner/src/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { Route } from 'react-router-dom';
+import ModalContainer from "./components/Modal/ModalContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <Route path="/" component={ModalContainer} />
       </div>
     );
   }
