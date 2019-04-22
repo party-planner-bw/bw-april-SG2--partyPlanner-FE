5402b71f51b43c259895fac0f9fec21053eceeea
Brennon Nuckols <brennuck@gmail.com>
2019-04-22 13:12:33 -0600

    very simple sign up page

--- party-planner/src/App.js
+++ party-planner/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 
 import { Route } from 'react-router-dom';
+
 import ModalContainer from "./components/Modal/ModalContainer";
+import PartyList from './components/PartyList';
+import SignUpPage from "./components/SignUpPage";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Route path="/" component={ModalContainer} />
+        <Route path="/signUpPage" component={SignUpPage} />
+        <Route path="/partylist" component={PartyList} />
       </div>
     );
   }
