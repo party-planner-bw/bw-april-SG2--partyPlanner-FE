6372c2341c20fa2a7aff68bb6dcc3fcaa2c5a0dd
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-04-22 19:10:28 -0400

    added node modules. started on the PartyList

---
+++ party-planner/src/components/PartyList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PartyList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            parties: []
+        }
+    }
+}
+
+export default PartyList;
