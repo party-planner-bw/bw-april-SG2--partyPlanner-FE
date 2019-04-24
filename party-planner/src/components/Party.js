955ff9dfbe335c304b24cbcafaceb38cfa34876c
Destiny Tibbitts <dtibbs5145@gmail.com>
2019-04-24 16:15:33 -0400

    Filled in the PartyForm.js

--- party-planner/src/components/Party.js
+++ party-planner/src/components/Party.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import axios from 'axios';
 
 class Party extends React.Component {
-        state = {
-            parties: []
-        }
-    
-
-    componentDidMount() {
-        axios.get('https://party-planner-build-week.herokuapp.com/api/parties')
-        .then(res => {this.setState({parties: res.data}) })
-        .catch(err => console.log(err));
-    }
-
     render() {
-       return (
-           <div>
-               <h2>{this.props.parties.theme}</h2>
-
-           </div>
-       )
+        return (
+            <div>
+                <h2>{this.props.parties.theme}</h2>
+                <h2>{this.props.parties.date}</h2>
+                <h2>{this.props.parties.budget}</h2>
+                <h2>{this.props.parties.guestCount}</h2>
+            </div>
+        )
     }
 }
 
